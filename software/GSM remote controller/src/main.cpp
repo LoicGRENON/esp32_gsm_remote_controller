3229e603328ddedc052a40a6e0bb0d86b873e3c4
Loïc GRENON <loic.grenon@difoolou.net>
2022-07-09 15:31:12 +0200

    Fix indentation to 4 spaces

--- software/GSM remote controller/src/main.cpp
+++ software/GSM remote controller/src/main.cpp
@@ -23,75 +23,75 @@ QueueHandle_t qGsmEventData;
 String sGsmEventDataISR;
 
 void IRAM_ATTR ISR_GSM_RI(){
-  sGsmEventDataISR = "";
-  while(GsmSerial.available()) {
-    char c = GsmSerial.read();
-    sGsmEventDataISR += c;
-  }
+    sGsmEventDataISR = "";
+    while(GsmSerial.available()) {
+        char c = GsmSerial.read();
+        sGsmEventDataISR += c;
+    }
 
-  // Send the string pointer to queue for further use
-  String * pGsmEventData = &sGsmEventDataISR;
-  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  xQueueSendToBackFromISR(qGsmEventData, &pGsmEventData, &xHigherPriorityTaskWoken);
+    // Send the string pointer to queue for further use
+    String * pGsmEventData = &sGsmEventDataISR;
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xQueueSendToBackFromISR(qGsmEventData, &pGsmEventData, &xHigherPriorityTaskWoken);
 }
 
 void read_sms_message(int sms_index) {
-  // TODO: add a readSMS() declaration to get the senderID at once (one AT+CMGR command)
-  // Currently both readSMS and getSenderID send AT+CMGR commands which is slow
-  // => pass these variables as reference of the function
-  String SMS = modem.readSMS(sms_index);
-  String ID = modem.getSenderID(sms_index);
-  Serial.print("Message From : ");
-  Serial.println(ID);
-  Serial.println(" and the message is ");
-  Serial.println(SMS);
+    // TODO: add a readSMS() declaration to get the senderID at once (one AT+CMGR command)
+    // Currently both readSMS and getSenderID send AT+CMGR commands which is slow
+    // => pass these variables as reference of the function
+    String SMS = modem.readSMS(sms_index);
+    String ID = modem.getSenderID(sms_index);
+    Serial.print("Message From : ");
+    Serial.println(ID);
+    Serial.println(" and the message is ");
+    Serial.println(SMS);
 }
 
 void parse_gsm_event_message() {
-  String *pGsmEventData;
-  xQueueReceive(qGsmEventData, &pGsmEventData, portMAX_DELAY);
-  String sGsmEventData = *pGsmEventData;
+    String *pGsmEventData;
+    xQueueReceive(qGsmEventData, &pGsmEventData, portMAX_DELAY);
+    String sGsmEventData = *pGsmEventData;
 
-  if(sGsmEventData.indexOf("CMTI:") > 0) {  // New message has been received
-    Serial.print("New SMS arrived at index :- ");
-    //Serial.println(sGsmEventData);
-    // TODO: remove the need to use newMessageInterrupt()
-    int sms_index = modem.newMessageInterrupt(sGsmEventData);
-    Serial.println(sms_index);
-    read_sms_message(sms_index);
-  } else {
-    Serial.println("false alarm");
-    Serial.println(sGsmEventData);
-    return;
-  }
+    if(sGsmEventData.indexOf("CMTI:") > 0) {  // New message has been received
+        Serial.print("New SMS arrived at index :- ");
+        //Serial.println(sGsmEventData);
+        // TODO: remove the need to use newMessageInterrupt()
+        int sms_index = modem.newMessageInterrupt(sGsmEventData);
+        Serial.println(sms_index);
+        read_sms_message(sms_index);
+    } else {
+        Serial.println("false alarm");
+        Serial.println(sGsmEventData);
+        return;
+    }
 }
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Initializing Serial...");
+    Serial.begin(115200);
+    Serial.println("Initializing Serial...");
 
-  // TODO: Try using higher baudrate
-  // Maybe check using TinyGsmAutoBaud() - But not in production code !
-  GsmSerial.begin(9600, SWSERIAL_8N1, GSM_RX_PIN, GSM_TX_PIN);
-  Serial.println("Initializing GSM module...");
-  delay(100);
+    // TODO: Try using higher baudrate
+    // Maybe check using TinyGsmAutoBaud() - But not in production code !
+    GsmSerial.begin(9600, SWSERIAL_8N1, GSM_RX_PIN, GSM_TX_PIN);
+    Serial.println("Initializing GSM module...");
+    delay(100);
 
-  // Autobaud communication with modem
-  // TODO: wait for OK ?
-  GsmSerial.write("AT\r");
-  delay(1000);
-  
-  // Queue of string pointers to get the data sent by the modem on RI events
-  qGsmEventData = xQueueCreate(10, sizeof(String *));
-  // Enable interrupt from SIM808 GSM module
-  pinMode(GSM_INT_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(GSM_INT_PIN), ISR_GSM_RI, RISING);
+    // Autobaud communication with modem
+    // TODO: wait for OK ?
+    GsmSerial.write("AT\r");
+    delay(1000);
+    
+    // Queue of string pointers to get the data sent by the modem on RI events
+    qGsmEventData = xQueueCreate(10, sizeof(String *));
+    // Enable interrupt from SIM808 GSM module
+    pinMode(GSM_INT_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(GSM_INT_PIN), ISR_GSM_RI, RISING);
 
-  Serial.println("Initializing done");
+    Serial.println("Initializing done");
 }
 
 // TODO: TinyGSM is a blocking library so use FreeRTOS and tasks to use the second core of ESP32
 void loop() {
-  parse_gsm_event_message();
-  delay(500);
+    parse_gsm_event_message();
+    delay(500);
 }
